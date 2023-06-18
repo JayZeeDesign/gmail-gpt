945e4b6b63148076de45fd188ab7d4a180993eff
Jason Zhou <jason.zhou.design@gmail.com>
2023-06-18 12:15:33 +1000

    added email analyse feature

--- main.py
+++ main.py
@@ -47,66 +47,9 @@ function_descriptions = [
 ]
 
 
-# email = """
-# Cyriac Kodath <cyriac.kodath@gmail.com>
-# Sat, 17 Jun, 09:38 (1 day ago)
-# to Jason.zhou.design
-
-# Hello Jason, 
-
-# I got your email from your youtube channel (really great videos by the way!) and wanted to write to you about using Chat GPT in Algorithmic Trading. 
-
-# A little intro about me, I have just finished a PhD in Economics from the UK a few months back and have experience with Quantitative models. I also have a basic knowledge of Python coding so have been able to follow your videos. Recently I released a course on Udemy about electronic market making and also set up a trading livestream as a proof of concept on my website (I will put the links below for you to take a look at). I was interested to scale this up and possibly improve it with AI models, though I am not quite sure how to go about this. Would love to have a chat about this, if you are interested?
-
-# Best wishes,
-
-# Cyriac Kodath
-
-# Udemy Course: https://www.udemy.com/course/electronic-market-making-with-angel-one-smart-api/?referralCode=1B5D076540D2986BEFEF
-# LinkedIn: https://www.linkedin.com/in/cyriackodath/
-# YouTube Channel: https://www.youtube.com/@AlgoTraderOnline/streams
-# Website: https://www.algotrader.online/
-# """
-
-# query = f"Please extract key information from this email: {email} "
-
-# messages = [{"role": "user", "content": query}]
-
-# response = openai.ChatCompletion.create(
-#     model="gpt-4-0613",
-#     messages=messages,
-#     functions = function_descriptions,
-#     function_call="auto"
-# )
-
-# print(response)
-
-
 class Email(BaseModel):
     from_email: str
     content: str
-
-
-emails = {
-    0: Email(from_email="cyriac.kodath@gmail.com",
-             content = """
-                Hello Jason, 
-
-                I got your email from your youtube channel (really great videos by the way!) and wanted to write to you about using Chat GPT in Algorithmic Trading. 
-
-                A little intro about me, I have just finished a PhD in Economics from the UK a few months back and have experience with Quantitative models. I also have a basic knowledge of Python coding so have been able to follow your videos. Recently I released a course on Udemy about electronic market making and also set up a trading livestream as a proof of concept on my website (I will put the links below for you to take a look at). I was interested to scale this up and possibly improve it with AI models, though I am not quite sure how to go about this. Would love to have a chat about this, if you are interested?
-
-                Best wishes,
-
-                Cyriac Kodath
-
-                Udemy Course: https://www.udemy.com/course/electronic-market-making-with-angel-one-smart-api/?referralCode=1B5D076540D2986BEFEF
-                LinkedIn: https://www.linkedin.com/in/cyriackodath/
-                YouTube Channel: https://www.youtube.com/@AlgoTraderOnline/streams
-                Website: https://www.algotrader.online/
-             """
-             )
-}
 
 
 @app.get("/")
@@ -116,15 +59,16 @@ def read_root():
 
 @app.post("/")
 def analyse_email(email: Email):
-    # query = f"Please extract key information from this email: {email} "
+    content = email.content
+    query = f"Please extract key information from this email: {content} "
 
-    # messages = [{"role": "user", "content": query}]
+    messages = [{"role": "user", "content": query}]
 
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-4-0613",
-    #     messages=messages,
-    #     functions = function_descriptions,
-    #     function_call="auto"
-    # )
+    response = openai.ChatCompletion.create(
+        model="gpt-4-0613",
+        messages=messages,
+        functions = function_descriptions,
+        function_call="auto"
+    )
 
     return {"email": email}
