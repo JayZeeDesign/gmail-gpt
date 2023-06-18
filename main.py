0be98d2a6516507e6b3fded486289de3740a4c3b
Jason Zhou <jason.zhou.design@gmail.com>
2023-06-18 16:11:56 +1000

    new function descriptions

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import openai
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 load_dotenv()
@@ -20,43 +19,40 @@ function_descriptions = [
                 "companyName": {
                     "type": "string",
                     "description": "the name of the company that sent the email"
-                },
-                "useCase": {
-                    "type": "string",
-                    "description": "The purpose & use case of this company's enquiry"
-                },
-                "contactDetails": {
-                    "type": "string",
-                    "description": "The contact details of the person who sent the email"
-                },
+                },                        
                 "priority": {
                     "type": "string",
-                    "description": "Try to give a priority score to this email based on how likely this email will leads to a good business opportunity for AI consulting, from 0 to 10; 10 most important"
+                    "description": "Try to give a priority score to this email based on how likely this email will leads to a good business opportunity, from 0 to 10; 10 most important"
                 },
                 "category": {
                     "type": "string",
-                    "description": "Try to categorise this email into categories like those: 1. customer support; 2. consulting; 3. job; 4. partnership; etc."
+                    "description": "Try to categorise this email into categories like those: 1. Sales 2. customer support; 3. consulting; 4. partnership; etc."
+                },
+                "product": {
+                    "type": "string",
+                    "description": "Try to identify which product the client is interested in, if any"
+                },
+                "amount":{
+                    "type": "string",
+                    "description": "Try to identify the amount of products the client wants to purchase, if any"
                 },
                 "nextStep":{
                     "type": "string",
                     "description": "What is the suggested next step to move this forward?"
                 }
             },
-            "required": ["companyName", "useCase", "contactDetails", "priority", "category", "nextStep"]
+            "required": ["companyName", "amount", "product", "priority", "category", "nextStep"]
         }
     }
 ]
-
 
 class Email(BaseModel):
     from_email: str
     content: str
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.post("/")
 def analyse_email(email: Email):
@@ -74,17 +70,18 @@ def analyse_email(email: Email):
 
     arguments = response.choices[0]["message"]["function_call"]["arguments"]
     companyName = eval(arguments).get("companyName")
-    useCase = eval(arguments).get("useCase")
-    contactDetails = eval(arguments).get("contactDetails")
     priority = eval(arguments).get("priority")
+    product = eval(arguments).get("product")
+    amount = eval(arguments).get("amount")
     category = eval(arguments).get("category")
     nextStep = eval(arguments).get("nextStep")
 
     return {
         "companyName": companyName,
-        "useCase": useCase,
-        "contactDetails": contactDetails,
+        "product": product,
+        "amount": amount,
         "priority": priority,
         "category": category,
         "nextStep": nextStep
     }
+
