6a9e970ece8e85b6435abb5cea31119a82e316c9
Jason Zhou <jason.zhou.design@gmail.com>
2023-07-02 10:19:57 +1000

    updated

--- main.py
+++ main.py
@@ -19,15 +19,7 @@ function_descriptions = [
                 "companyName": {
                     "type": "string",
                     "description": "the name of the company that sent the email"
-                },                        
-                "priority": {
-                    "type": "string",
-                    "description": "Try to give a priority score to this email based on how likely this email will leads to a good business opportunity, from 0 to 10; 10 most important"
-                },
-                "category": {
-                    "type": "string",
-                    "description": "Try to categorise this email into categories like those: 1. Sales 2. customer support; 3. consulting; 4. partnership; etc."
-                },
+                },                                        
                 "product": {
                     "type": "string",
                     "description": "Try to identify which product the client is interested in, if any"
@@ -36,52 +28,93 @@ function_descriptions = [
                     "type": "string",
                     "description": "Try to identify the amount of products the client wants to purchase, if any"
                 },
+                "category": {
+                    "type": "string",
+                    "description": "Try to categorise this email into categories like those: 1. Sales 2. customer support; 3. consulting; 4. partnership; etc."
+                },
                 "nextStep":{
                     "type": "string",
                     "description": "What is the suggested next step to move this forward?"
-                }
+                },
+                "priority": {
+                    "type": "string",
+                    "description": "Try to give a priority score to this email based on how likely this email will leads to a good business opportunity, from 0 to 10; 10 most important"
+                },
             },
             "required": ["companyName", "amount", "product", "priority", "category", "nextStep"]
         }
     }
 ]
 
-class Email(BaseModel):
-    from_email: str
-    content: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+email = """
+Dear Jason 
+I hope this message finds you well. I'm Shirley from Gucci;
 
-@app.post("/")
-def analyse_email(email: Email):
-    content = email.content
-    query = f"Please extract key information from this email: {content} "
+I'm looking to purchase some company T-shirt for my team, we are a team of 100k people, and we want to get 2 t-shirt per personl
 
-    messages = [{"role": "user", "content": query}]
+Please let me know the price and timeline you can work with;
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4-0613",
-        messages=messages,
-        functions = function_descriptions,
-        function_call="auto"
-    )
+Looking forward
 
-    arguments = response.choices[0]["message"]["function_call"]["arguments"]
-    companyName = eval(arguments).get("companyName")
-    priority = eval(arguments).get("priority")
-    product = eval(arguments).get("product")
-    amount = eval(arguments).get("amount")
-    category = eval(arguments).get("category")
-    nextStep = eval(arguments).get("nextStep")
+Shirley Lou
+"""
 
-    return {
-        "companyName": companyName,
-        "product": product,
-        "amount": amount,
-        "priority": priority,
-        "category": category,
-        "nextStep": nextStep
-    }
+prompt = f"Please extract key information from this email: {email} "
+message = [{"role": "user", "content": prompt}]
+
+response = openai.ChatCompletion.create(
+    model="gpt-4-0613",
+    messages=message,
+    functions = function_descriptions,
+    function_call="auto"
+)
+
+print(response)
+
+
+
+
+
+
+
+
+# class Email(BaseModel):
+#     from_email: str
+#     content: str
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+# @app.post("/")
+# def analyse_email(email: Email):
+#     content = email.content
+#     query = f"Please extract key information from this email: {content} "
+
+#     messages = [{"role": "user", "content": query}]
+
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4-0613",
+#         messages=messages,
+#         functions = function_descriptions,
+#         function_call="auto"
+#     )
+
+#     arguments = response.choices[0]["message"]["function_call"]["arguments"]
+#     companyName = eval(arguments).get("companyName")
+#     priority = eval(arguments).get("priority")
+#     product = eval(arguments).get("product")
+#     amount = eval(arguments).get("amount")
+#     category = eval(arguments).get("category")
+#     nextStep = eval(arguments).get("nextStep")
+
+#     return {
+#         "companyName": companyName,
+#         "product": product,
+#         "amount": amount,
+#         "priority": priority,
+#         "category": category,
+#         "nextStep": nextStep
+#     }
 
